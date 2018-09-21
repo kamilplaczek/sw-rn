d0918b27b65b4080111b300c8ff886537cd20f5d
Kamil Placzek <kamil.placzek@thesoftwarehouse.pl>
2018-09-21 16:39:20 +0200

    Unstyled SW movie list.

--- src/app/home/list/list.component.js
+++ src/app/home/list/list.component.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
 export class List extends Component {
+  state = {
+    films: [],
+  };
+
   static navigationOptions = {
     tabBarIcon: () => <Text>🎬</Text>,
   };
 
+  componentDidMount = async () => {
+    const filmsResult = await fetch('https://swapi.co/api/films');
+    const filmsData = await filmsResult.json();
+    this.setState({
+      films: filmsData.results.map(film => ({
+        releaseDate: film.release_date,
+        director: film.director,
+        title: film.title,
+        opening: film.opening_crawl,
+        id: film.episode_id,
+      })),
+    });
+  };
+
+  renderFilm = ({ item }) => (
+    <View style={{ padding: 20 }}>
+      <Text>{item.title}</Text>
+      <Text>{item.director}</Text>
+      <Text>{item.releaseDate}</Text>
+    </View>
+  );
+
+  filmKeyExtractor = item => `${item.id}`;
+
   render() {
-    return <Text>List component!</Text>;
+    const { films } = this.state;
+    return films.length > 0 ? (
+      <View style={{ flex: 1 }}>
+        <FlatList style={{ flex: 1 }} keyExtractor={this.filmKeyExtractor} data={films} renderItem={this.renderFilm} />
+      </View>
+    ) : (
+      <ActivityIndicator size="large" style={{ marginTop: '60%' }} />
+    );
   }
 }
