d41c6ff3b685647d78cc82918e17bec3b8791271
Kamil Placzek <kamil.placzek@thesoftwarehouse.pl>
2018-09-22 14:34:36 +0200

    Add example animation.

--- src/app/home/details/details.component.js
+++ src/app/home/details/details.component.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import { styles } from './details.styles';
 import { SafeAreaView } from 'react-navigation';
-import { Text } from 'react-native';
+import { Text, Animated } from 'react-native';
 
 export class Details extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: navigation.state.params.film.title,
   });
 
+  state = {
+    position: new Animated.Value(400),
+  };
+
+  componentDidMount = () => {
+    Animated.timing(this.state.position, {
+      toValue: -300,
+      duration: 15000,
+      useNativeDriver: true,
+    }).start();
+  };
+
   render() {
     const { film } = this.props.navigation.state.params;
+    const { position } = this.state;
+
     return (
       <SafeAreaView style={styles.container}>
-        <Text style={styles.openingText}>{film.opening}</Text>
+        <Animated.Text
+          style={{
+            ...styles.openingText,
+            transform: [{ perspective: 400 }, { rotateX: '50deg' }, { translateY: position }],
+          }}
+        >
+          {film.opening}
+        </Animated.Text>
       </SafeAreaView>
     );
   }
--- src/app/home/details/details.styles.js
+++ src/app/home/details/details.styles.js
@@ -3,9 +3,14 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     padding: 20,
   },
-  openingText: {},
+  openingText: {
+    fontSize: 10,
+    color: 'yellow',
+    position: 'absolute',
+    padding: 50,
+  },
 });
